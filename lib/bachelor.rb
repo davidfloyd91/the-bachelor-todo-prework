12fa9a9fca6c5b0893d066a947bc43dfe11abc9b
davidfloyd91 <davidfloyd91@gmail.com>
2018-12-03 23:05:00 -0500

    Done.

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -1,19 +1,79 @@
 def get_first_name_of_season_winner(data, season)
-  # code here
+  first_name = nil
+
+  data.each do |a, b|
+    if a == season
+      b.each do |c|
+        if c["status"] == "Winner"
+          first_name = c["name"].split(" ")[0]
+        end
+      end
+    end
+  end
+
+  first_name
 end
 
 def get_contestant_name(data, occupation)
-  # code here
+  name = nil
+
+  data.each do |a, b|
+    b.each do |c|
+      if c["occupation"] == occupation
+        name = c["name"]
+      end
+    end
+  end
+
+  name
 end
 
 def count_contestants_by_hometown(data, hometown)
-  # code here
+  count = 0
+
+  data.each do |a, b|
+    b.each do |c|
+      if c["hometown"] == hometown
+        count += 1
+      end
+    end
+  end
+
+  count
 end
 
 def get_occupation(data, hometown)
-  # code here
+  occupation_array = []
+
+  data.each do |a, b|
+    b.each do |c|
+      if c["hometown"] == hometown
+        occupation_array << c["occupation"]
+      end
+    end
+  end
+
+  occupation_array[0]
 end
 
 def get_average_age_for_season(data, season)
-  # code here
+  ages = []
+
+  data.each do |a, b|
+    if a == season
+      b.each do |c|
+        ages << c["age"].to_f
+      end
+    end
+  end
+
+  sum = 0
+  i = 0
+
+  while i < ages.count
+    sum += ages[i]
+    i += 1
+  end
+
+  age = (sum / ages.count).round
 end
